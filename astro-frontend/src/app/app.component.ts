e7ba9a4ef466425b76023f3eada96cf60145a941
Sahsranshu1609 <pmsahsranshu@gmail.com>
2024-12-10 14:48:24 +0530

    added FooterComponent and CommonModule

--- astro-frontend/src/app/app.component.ts
+++ astro-frontend/src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // Import NgbModule
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { DailyHoroscopeComponent } from './daily-horoscope/daily-horoscope.component';
 import { BirthChartComponent } from './birth-chart/birth-chart.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { LoveCompatibilityComponent } from './love-compatibility/love-compatibility.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [BirthChartComponent, DailyHoroscopeComponent, HeaderComponent, LandingPageComponent, LoveCompatibilityComponent, RouterOutlet, RouterLink, RouterLinkActive, NgbModule],
+  imports: [
+    BirthChartComponent,
+    DailyHoroscopeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent,
+    LoveCompatibilityComponent,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    NgbModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'astro-frontend';
